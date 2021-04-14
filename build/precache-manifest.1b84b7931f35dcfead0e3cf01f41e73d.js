0d069807a5f77eba60c1cd8703b92a46a402c73e
Sujay Chathu <sujaychathu@imac.lan>
2021-04-14 16:28:57 -0700

    creating master branch to docker build

---
+++ build/precache-manifest.1b84b7931f35dcfead0e3cf01f41e73d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97cdba7374f3b0f5ba8e625741bc3411",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4276bc99dcfb8e1a1f21",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5c419ee7b9d4f583d0b9",
+    "url": "/static/css/main.941a7d5c.chunk.css"
+  },
+  {
+    "revision": "4276bc99dcfb8e1a1f21",
+    "url": "/static/js/2.adf17f9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.adf17f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c419ee7b9d4f583d0b9",
+    "url": "/static/js/main.50d3a116.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
